39c00380283e6d07dce062756cbf8611b3854fc7
gchance22 <Graham@Grahams-MacBook-Pro.local>
2015-08-28 11:48:59 -0400

    more implementation

--- Pod/Classes/Harrier.swift
+++ Pod/Classes/Harrier.swift
@@ -7,12 +7,11 @@
 //
 
 import Foundation
-import Reachability
 
 // Montagu's Harrier
 // "As this bird has a wide distribution, it will take whatever prey is available in the area where it nests [and add them to its task queue]" - Wikipedia
 //
-public class Harrier {
+public class Harrier: NSObject {
     
     /// Tasks waiting to be added to the activeQueue
     private var queuedTasks: [HarrierTask] = []
@@ -32,28 +31,70 @@ public class Harrier {
     public var maxConcurrentTasks: Int = 3
     
     init(maxConcurrentTasks: Int) {
+        super.init()
         self.maxConcurrentTasks = maxConcurrentTasks
-        activeQueue.maxConcurrentTasks = maxConcurrentTasks
+        activeQueue.maxConcurrentOperationCount = maxConcurrentTasks
+        setUpKVO()
     }
+    
+    private func setUpKVO() {
+        activeQueue.addObserver(self, forKeyPath: "operationCount", options: NSKeyValueObservingOptions.New  | NSKeyValueObservingOptions.Old, context: nil)
+    }
+    
+    deinit {
+        activeQueue.removeObserver(self, forKeyPath: "operationCount")
+    }
+    
+    private func dequeueNextTask() {
+        if queuedTasks.count > 0 {
+            queuedTasks.removeLast()
+        }
+    }
+    
+    private func dequeueTask(task: HarrierTask) {
+        activeTasks.append(task)
+        activeQueue.addOperation(task.operation)
+    }
+    
+    private func sortQueue() {
+        // remember the array is ordered low priority first
+        queuedTasks.sort { !$0.isHigherPriority(thanTask: $1) }
+    }
+    
+    public override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if let queue = object as? NSOperationQueue {
+            if keyPath == "operationCount" {
+                if activeTasks.count < maxConcurrentTasks && suspended {
+                    dequeueNextTask()
+                }
+            }
+        }
+    }
+
+    
+    // MARK: Public API
  
     /**
     Stop adding tasks to the activeQueue.
     */
-    func pause() {
+    public func pause() {
         paused = true
     }
     
     /**
     Start adding tasks to the activeQueue again.
     */
-    func restart() {
+    public func start() {
         paused = false
+        if activeTasks.count < maxConcurrentTasks {
+            dequeueNextTask()
+        }
     }
     
     /**
     Cancel all tasks in activeQueue and empty queuedTasks.
     */
-    func cancelAll() {
+    public func cancelAll() {
         queuedTasks = []
         activeTasks = []
         for operation in activeQueue.operations {
@@ -63,20 +104,19 @@ public class Harrier {
         }
     }
     
+    /**
+    Queues up the task.
     
-    func enqueueTask(task: HarrierTask) {
+    :param: task Task to be queued.
+    */
+    public func enqueueTask(task: HarrierTask) {
         queuedTasks.append(task)
+        sortQueue()
+        if activeTasks.count < maxConcurrentTasks && suspended {
+            dequeueNextTask()
+        }
     }
 
-    func dequeueNextTask() {
-        
-    }
-    
-    func dequeueTask(task: HarrierTask) {
-        activeTasks.append(task)
-        activeQueue.addOperation(task.operation)
-    }
-    
     
     
 }
@@ -87,16 +127,23 @@ public class HarrierTask {
     let usesNetwork: Bool
     
     /// The priority measurement that ranks above all others. 0 (low priority) - infinity (high priority)
-    let basePriortity: Int
+    public let basePriority: Int
     
-    let operation = NSOperation()
+    let operation: NSOperation
     
-    let dateCreated: NSTimeInterval!
+    let dateCreated: NSTimeInterval
     
-    public init(basePriority: Int, usesNetwork: Bool = true) {
-        self.basePriortity = basePriority
+    var retryCount: Int
+    
+    public init(operation: NSOperation, basePriority: Int, usesNetwork: Bool = true) {
+        self.basePriority = basePriority
         self.usesNetwork = usesNetwork
-        //self.dateCreated = NSTimeInterval
+        self.dateCreated = NSDate().timeIntervalSince1970
+        self.operation = operation
+        self.retryCount = 0
     }
     
+    public func isHigherPriority(thanTask other: HarrierTask) -> Bool {
+        return basePriority > other.basePriority || (basePriority == other.basePriority && retryCount < other.retryCount && dateCreated < other.dateCreated)
+    }
 }
