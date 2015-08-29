de13ed1a356b876047c2129b812459e7f141c5c3
gchance22 <Graham@Grahams-MacBook-Pro-2.local>
2015-08-29 13:39:32 -0400

    Some tests and queue implementation

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -4,9 +4,12 @@ import Harrier
 
 class Tests: XCTestCase {
     
+    var harrier: Harrier!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        harrier = Harrier()
     }
     
     override func tearDown() {
@@ -26,4 +29,23 @@ class Tests: XCTestCase {
         }
     }
     
+    func testQueuingProcess() {
+        let operation = NSOperation()
+        let task = HarrierTask(name: "", basePriority: 0, taskAttributes: [:])
+        harrier.enqueueTask(task)
+        XCTAssert(harrier.taskCount > 0, "Task was not queued properly")
+    }
+    
+    
+    func testPausingAndRestarting() {
+        let operation = NSOperation()
+        let task = HarrierTask(name: "", basePriority: 0, taskAttributes: [:])
+        harrier.pause()
+        harrier.enqueueTask(task)
+        XCTAssert(!harrier.running && harrier.runningTasks.count == 0, "Harrier did not pause")
+        harrier.restart()
+        XCTAssert(harrier.running && harrier.runningTasks.count > 0, "Harrier did not restart")
+
+    }
+
 }
--- Pod/Classes/Harrier.swift
+++ Pod/Classes/Harrier.swift
@@ -20,20 +20,45 @@ public class Harrier: NSObject {
     private let activeQueue = NSOperationQueue()
     
     /// Should be the task equivalent of activeQueue.
-    private var activeTasks: [HarrierTask] = []
+    private var activeTasks: [HarrierTask] {
+        return activeQueue.operations as? [HarrierTask] ?? []
+    }
     
     private var paused: Bool = false
     
-    public var suspended: Bool {
+    private var _maxConcurrentTasks: Int {
+        didSet {
+            activeQueue.maxConcurrentOperationCount = _maxConcurrentTasks
+        }
+    }
+    
+    public var maxConcurrentTasks: Int {
+        return _maxConcurrentTasks
+    }
+    
+    public var running: Bool {
         return !paused
     }
     
-    public var maxConcurrentTasks: Int = 3
+    public var tasks: [HarrierTask] {
+        var tasks = queuedTasks
+        for task in activeTasks {
+            tasks.append(task)
+        }
+        return tasks
+    }
     
-    init(maxConcurrentTasks: Int) {
+    public var runningTasks: [HarrierTask] {
+        return activeTasks
+    }
+    
+    public var taskCount: Int {
+        return queuedTasks.count + activeTasks.count
+    }
+    
+    public override init() {
+        self._maxConcurrentTasks = 3
         super.init()
-        self.maxConcurrentTasks = maxConcurrentTasks
-        activeQueue.maxConcurrentOperationCount = maxConcurrentTasks
         setUpKVO()
     }
     
@@ -47,13 +72,12 @@ public class Harrier: NSObject {
     
     private func dequeueNextTask() {
         if queuedTasks.count > 0 {
-            queuedTasks.removeLast()
+            dequeueTask(queuedTasks.removeLast())
         }
     }
     
     private func dequeueTask(task: HarrierTask) {
-        activeTasks.append(task)
-        activeQueue.addOperation(task.operation)
+        activeQueue.addOperation(task)
     }
     
     private func sortQueue() {
@@ -64,7 +88,7 @@ public class Harrier: NSObject {
     public override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
         if let queue = object as? NSOperationQueue {
             if keyPath == "operationCount" {
-                if activeTasks.count < maxConcurrentTasks && suspended {
+                if activeTasks.count < maxConcurrentTasks && running {
                     dequeueNextTask()
                 }
             }
@@ -84,7 +108,7 @@ public class Harrier: NSObject {
     /**
     Start adding tasks to the activeQueue again.
     */
-    public func start() {
+    public func restart() {
         paused = false
         if activeTasks.count < maxConcurrentTasks {
             dequeueNextTask()
@@ -96,7 +120,6 @@ public class Harrier: NSObject {
     */
     public func cancelAll() {
         queuedTasks = []
-        activeTasks = []
         for operation in activeQueue.operations {
             if let operation = operation as? NSOperation {
                 operation.cancel()
@@ -105,45 +128,117 @@ public class Harrier: NSObject {
     }
     
     /**
-    Queues up the task.
+    Queues up the task. Note: To prevent queueing duplicates, use enqueueTask()
     
-    :param: task Task to be queued.
+    :param: task Task to queued.
     */
     public func enqueueTask(task: HarrierTask) {
         queuedTasks.append(task)
         sortQueue()
-        if activeTasks.count < maxConcurrentTasks && suspended {
+        if activeTasks.count < maxConcurrentTasks && running {
             dequeueNextTask()
         }
     }
 
+    /**
+    Enqueues a task if a task with the same uniqueIdentifier does not already exist in the queue.
+    
+    :param: task Task to enqueue.
+    */
+    public func enqueueUniqueTask(task: HarrierTask) {
+        if !contains(tasks, task) {
+            enqueueTask(task)
+        } else {
+            println("Not enqueuing task because it is already in the queue. Task ID: \(task.uniqueIdentifier)")
+        }
+    }
     
     
 }
 
+public enum HarrierTaskCompletionStatus: String {
+    case Incomplete = "Incomplete",
+    Success = "Success",
+    Failure = "Failure",
+    Abandon = "Abandon",
+    Canceled = "Canceled"
+}
 
-public class HarrierTask {
 
-    let usesNetwork: Bool
+
+public class HarrierTask: NSOperation {
+
     
     /// The priority measurement that ranks above all others. 0 (low priority) - infinity (high priority)
     public let basePriority: Int
     
-    let operation: NSOperation
+    public let dateCreated: NSTimeInterval
     
-    let dateCreated: NSTimeInterval
+    public var retryCount: Int
     
-    var retryCount: Int
+    public let taskAttributes: [String: String]
     
-    public init(operation: NSOperation, basePriority: Int, usesNetwork: Bool = true) {
+    private var _finished = false
+    private var _executing = false
+    private var _completionStatus: HarrierTaskCompletionStatus?
+    
+    public var completionStatus: HarrierTaskCompletionStatus? {
+        return _completionStatus
+    }
+    
+    override public var executing: Bool {
+        get { return _executing }
+        set {
+            willChangeValueForKey("isExecuting")
+            _executing = newValue
+            didChangeValueForKey("isExecuting")
+        }
+    }
+    
+    override public var finished:Bool {
+        get { return _finished }
+        set {
+            willChangeValueForKey("isFinished")
+            _finished = newValue
+            didChangeValueForKey("isFinished")
+        }
+    }
+    
+    public var uniqueIdentifier: String {
+        var identifier = "HarrierTask"
+        for (key, value) in taskAttributes {
+            identifier = identifier + "-\(key):\(value)"
+        }
+        return identifier
+    }
+    
+    public init(basePriority: Int, taskAttributes: [String: String]) {
         self.basePriority = basePriority
-        self.usesNetwork = usesNetwork
         self.dateCreated = NSDate().timeIntervalSince1970
-        self.operation = operation
         self.retryCount = 0
+        self.taskAttributes = taskAttributes
+        super.init()
     }
     
     public func isHigherPriority(thanTask other: HarrierTask) -> Bool {
         return basePriority > other.basePriority || (basePriority == other.basePriority && retryCount < other.retryCount && dateCreated < other.dateCreated)
     }
+    
+    public override func cancel() {
+        super.cancel()
+        _completionStatus = .Canceled
+        finished = true
+    }
+    
+    public func completeWithStatus(status: HarrierTaskCompletionStatus) {
+        _completionStatus = status
+        finished = true
+    }
+    
 }
+
+public func ==(lhs: HarrierTask, rhs: HarrierTask) -> Bool {
+    return lhs.uniqueIdentifier == rhs.uniqueIdentifier
+}
+
+
