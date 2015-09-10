d7f4fb10729b4a432718ec4ea28b96bbf81ee604
Graham <graham@doublebluesports.com>
2015-09-10 19:07:54 -0400

    Removed FMDB as a pod

--- Example/Pods/Target Support Files/Pods-Harrier_Tests/Pods-Harrier_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-Harrier_Tests/Pods-Harrier_Tests-frameworks.sh
@@ -52,10 +52,8 @@ code_sign_if_enabled() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework 'Pods-Harrier_Tests/FMDB.framework'
   install_framework 'Pods-Harrier_Tests/HarrierQueue.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework 'Pods-Harrier_Tests/FMDB.framework'
   install_framework 'Pods-Harrier_Tests/HarrierQueue.framework'
 fi
