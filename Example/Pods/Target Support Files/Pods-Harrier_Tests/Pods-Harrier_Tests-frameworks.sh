80f23116f4d7df2bc6cb79a5aa4d51adb964f4ec
gchance22 <graham@doublebluesports.com>
2016-02-16 13:03:02 -0500

    Updated podspec

--- Example/Pods/Target Support Files/Pods-Harrier_Tests/Pods-Harrier_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-Harrier_Tests/Pods-Harrier_Tests-frameworks.sh
@@ -85,9 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-Harrier_Tests/HarrierQueue.framework"
-  install_framework "Pods-Harrier_Tests/SQLite.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-Harrier_Tests/HarrierQueue.framework"
-  install_framework "Pods-Harrier_Tests/SQLite.framework"
 fi
